cb9d9a5ff03a299535df8755fa685a867f0e2766
AndarilhoL <leonardosantos2742@gmail.com>
2023-04-16 13:15:43 -0300

    Subindo começo do desafio 1, criando classe específica para fazer toda a regra de negócio separada por métodos

---
+++ Desafio_1/Conversor.cs
@@ -0,0 +1,38 @@
+﻿namespace Desafio_1
+{
+    public static class Conversor
+    {
+        public static string ValidarNumero(string numero)
+        {
+            bool numeroValido = int.TryParse(numero, out int valorNumericoInteiro);
+
+            if (numeroValido && valorNumericoInteiro == 0)
+                return "O número digitado por extenso é Zero";
+
+            if (numeroValido && valorNumericoInteiro != 0)
+                return SepararNumeroParaValidar(numero).ToString();
+
+            return "O valor informado não é um inteiro ou não representa um valor aceitável";
+        }
+
+        private static char[] ConverterInteiroParaExtenso(string numeroAjustado)
+        {
+            var numeroSeparado = numeroAjustado.ToCharArray();
+        
+        
+        
+            return numeroSeparado;
+        }
+
+        private static char[] SepararNumeroParaValidar(string numeroAjustado)
+        {
+            var numeroSeparado = numeroAjustado.ToCharArray();
+
+
+
+            return numeroSeparado;
+        }
+
+        
+    }
+}
