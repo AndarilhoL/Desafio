5067ac362e4cba7ec5325b96e284acc6a52bdd5f
AndarilhoL <leonardosantos2742@gmail.com>
2023-04-19 16:28:30 -0300

    criando segundo desafio

---
+++ Desafio_2/Program.cs
@@ -0,0 +1,2 @@
+﻿Console.WriteLine("Insira a quantidade de números inteiros que deseja somar (lembrando que essa quantidade pode ter até 1 milhão de números):");
+var valorInseridoValido = int.TryParse(Console.ReadLine(), out var valor);
