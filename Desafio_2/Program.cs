a013a013fe2fdbf4c76a168c56b3c211607cbe05
AndarilhoL <leonardosantos2742@gmail.com>
2023-04-19 16:29:13 -0300

    subindo segundo desafio com tratamento de inteiro

--- Desafio_2/Program.cs
+++ Desafio_2/Program.cs
@@ -1,2 +1,3 @@
 ﻿Console.WriteLine("Insira a quantidade de números inteiros que deseja somar (lembrando que essa quantidade pode ter até 1 milhão de números):");
 var valorInseridoValido = int.TryParse(Console.ReadLine(), out var valor);
+
